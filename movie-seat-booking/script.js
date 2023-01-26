1576e7dcad23a8a8f559a44d726d27543632cf9a
multida <wow_da65@naver.com>
2023-01-26 21:57:40 +0900

    🚧 한 영화에 대한 좌석 선택 및 좌석 수, 가격 업데이트

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+const ticketPrice = +movieSelect.value; //앞에 + 넣어주면 type이 숫자로 변함!
+
+//Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+// seats.forEach((seat) => {
+//   seat.addEventListener("click", function () {
+//     seat.classList.toggle("selected");
+//   });
+// });
