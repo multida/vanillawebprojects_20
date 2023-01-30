9aa5e9fc7d3e98dce2b9c82d58bb300aa4caa643
multida <wow_da65@naver.com>
2023-01-30 22:43:57 +0900

    🚧 forEach를 사용해서 이름과 돈을 불러왔고, element를 생성해서 넣어주었다. 돈은 콤마와 달러를 추가했고, 유저버튼을 클릭했을때 랜덤으로 계쏙 나오게 만듦

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -8,6 +8,8 @@ const calculateWealthBtn = document.getElementById("calculate-wealth");
 let data = [];
 
 getRandomUser();
+getRandomUser();
+getRandomUser();
 
 // fetch random user and add money
 async function getRandomUser() {
@@ -20,7 +22,7 @@ async function getRandomUser() {
   console.log(user);
 
   const newUser = {
-    name: `${user.name.first} ${user.namelast}`,
+    name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
   };
 
@@ -32,4 +34,34 @@ async function getRandomUser() {
 //Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+//Update DOM
+function updateDOM(provideData = data) {
+  //Clear main div
+  main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+
+  console.log(provideData);
+
+  provideData.forEach((item) => {
+    console.log(item);
+
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+
+    main.appendChild(element);
+  });
+}
+
+//Format number as money
+function formatMoney(number) {
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"); // 12,345.67
+}
+
+//Event add listener
+addUserBtn.addEventListener("click", getRandomUser);
