90c4adcaf86c1d8d415c6dff8aa90c3faadf5412
multida <wow_da65@naver.com>
2023-02-05 11:34:33 +0900

    🚧 Keydown letter press && Show notification

--- hangman/script.js
+++ hangman/script.js
@@ -19,20 +19,22 @@ const words = [
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ["w", "i", "z", "a", "r", "d"];
+const correctLetters = [];
 const wrongLetters = [];
 
 //Show hidden word
 function displayWord() {
-  wordEl.innerHTML = `${selectedWord
-    .split("")
-    .map(
-      (letter) =>
-        `<span class="letter">${
-          correctLetters.includes(letter) ? letter : ""
-        }</span>`
-    )
-    .join("")}`;
+  wordEl.innerHTML = `${
+    selectedWord
+      .split("")
+      .map(
+        (letter) =>
+          `<span class="letter">${
+            correctLetters.includes(letter) ? letter : ""
+          }</span>`
+      )
+      .join("") //다시 빈무열자로 바꿔줌?
+  }`;
 
   const innerWord = wordEl.innerText.replace(/\n/g, ""); //떨어져 나오던 문자들을 한줄에 나오도록 정규식으로 바꿔준다.
   console.log(wordEl.innerText, innerWord); //wordEl.innerText: 단어의 문자들이 떨어져서 나옴
@@ -42,5 +44,43 @@ function displayWord() {
     popup.style.display = "flex";
   }
 }
+
+//Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+//Show notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener("keydown", (e) => {
+  //console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter))
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+  } else {
+    if (!wrongLetters.includes(letter)) {
+      wrongLetters.push(letter);
+
+      updateWrongLettersEl();
+    } else {
+      showNotification();
+    }
+  }
+});
 
 displayWord();
